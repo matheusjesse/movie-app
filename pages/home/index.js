33a55f0616f67e57f3b86c80468cda4cfa644020
matheusjesse <matheus_jesse@hotmail.com>
2023-01-11 16:15:43 -0300

    feat: configuração inicial do app e instalação do StyledComponents

---
+++ pages/home/index.js
@@ -0,0 +1,12 @@
+import HomeContainer from "./style"
+
+function Home() {
+  
+    return (
+      <HomeContainer>
+        <h1>FILMES POPULARES</h1>
+      </HomeContainer>
+    );
+  }
+  
+  export default Home;
---
+++ pages/home/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+    background-color: #020006;
+    width: 100vw;
+    height: 100vh;
+  h1 {
+    text-align: center;
+    color: white;
+  }
+`;
+
+export default HomeContainer;
